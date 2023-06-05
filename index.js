763b2fa64c16a859cc147b25b474118f60ac80c8
RobertoValente2 <robertovalentecontato@gmail.com>
2023-06-05 09:16:36 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const vCardGenerator = require('vcards-js');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    let vCard = vCardGenerator();
+    vCard.firstName = "Aaa";
+    vCard.lastName = "Bbb";
+    vCard.title = "Ccc";
+    vCard.email = "Ddd@gmail.com";
+    vCard.cellPhone = "912345678";
+    vCard.socialUrls['instagram'] = "instagram.com/roberto.valennte";
+    vCard.version = "3.0";
+
+    let vCardData = vCard.getFormattedString;
+    res.render('index', {vCard: vCardData});
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
